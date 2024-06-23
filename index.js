07223f26100bc019e9a710a05981702c11f7c0b5
Saad Layachi <120133971+Saadlayachi@users.noreply.github.com>
2024-06-23 19:32:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("I LOVE PIZZA");
+window.alert("I really love pizza");
+
+
+
+// This is a comment 
+/*
+ this 
+ is 
+ multiline
+ comment
+*/
+let FirstName = "BRO";
+let age = 21;
+age = age + 1;
+let student = false;
+
+
+console.log(FirstName);
+console.log("You are",age,"years old");
+console.log(student);
+
+document.getElementById("p1").innerHTML = "hello " + FirstName;
+document.getElementById("p2").innerHTML = "You are " +age+ " Years old ";
+
+  
